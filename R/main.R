72b32e71941b3068c08923f9eaf41363101b7602
Brian OMeara <omeara.brian@gmail.com>
2017-12-07 13:15:04 -0500

    update with publications

--- R/main.R
+++ R/main.R
@@ -2,8 +2,10 @@ library(twitteR)
 library(rotl)
 library(rgbif)
 library(maps)
-source('.secret.R')
-setup_twitter_oauth(consumer.key, consumer.secret)
+library(rdryad)
+library(treebase)
+source('.secret.R') #includes authorization
+#setup_twitter_oauth(consumer.key, consumer.secret)
 my.timeline <- userTimeline("phylobot", n=1)
 max.ID <- as.data.frame(my.timeline[[1]])$id
 
@@ -11,19 +13,19 @@ ProcessTweet <- function(x) {
 	tweet.df <- as.data.frame(x)
 	print(Sys.time())
 	print(tweet.df)
-	if(grepl('#icanhaztree', tweet.df$text, ignore.case=TRUE)) {
+	if(grepl('#icanhaztree', tweet.df$text, ignore.case=TRUE) | grepl('#tree', tweet.df$text, ignore.case=TRUE)) {
 		ProcessICanHazTree(tweet.df)
-	} else if(grepl('#icanhazncbi', tweet.df$text, ignore.case=TRUE)) {
+	} else if(grepl('#icanhazncbi', tweet.df$text, ignore.case=TRUE) | grepl('#ncbi', tweet.df$text, ignore.case=TRUE)) {
 		ProcessICanHazNCBI(tweet.df)
-	} else if(grepl('#icanhazeol', tweet.df$text, ignore.case=TRUE)) {
+	} else if(grepl('#icanhazeol', tweet.df$text, ignore.case=TRUE) | grepl('#eol', tweet.df$text, ignore.case=TRUE)) {
 		ProcessICanHazEOL(tweet.df)
-	} else if(grepl('#icanhazmap', tweet.df$text, ignore.case=TRUE)) {
+	} else if(grepl('#icanhazmap', tweet.df$text, ignore.case=TRUE) | grepl('#map', tweet.df$text, ignore.case=TRUE)) {
                 ProcessICanHazMap(tweet.df)
-        } else if(grepl('#icanhazjob', tweet.df$text, ignore.case=TRUE)) {
-		return.tweet <- paste('@', tweet.df$screenName, ' see jobs at https://sites.google.com/site/wikibiologypostings/home/2015-2016-wiki-biology-jobs, @evoldir, etc.', sep="")
+  } else if(grepl('#icanhazjob', tweet.df$text, ignore.case=TRUE) | grepl('#job', tweet.df$text, ignore.case=TRUE)) {
+		return.tweet <- paste('@', tweet.df$screenName, ' see jobs at https://docs.google.com/spreadsheets/d/1XcEuPa7YPSHaw11OB9sPKZfQ8ZCYrdiMKzQZC8QSN7s/edit#gid=1240849863, @evoldir, etc.', sep="")
                updateStatus(return.tweet, inReplyTo=tweet.df$replyToSID, bypassCharLimit=TRUE)
 	} else {
-		#some sort of failover: perhaps talk to hubot	
+		#some sort of failover: perhaps talk to hubot
 	}
 	Sys.sleep(5)
 	return(as.numeric(tweet.df$id))
@@ -31,10 +33,45 @@ ProcessTweet <- function(x) {
 
 #https://tree.opentreeoflife.org/opentree/argus/ottol@5342306/Anolis
 ProcessICanHazTree <- function(x) {
-	if(grepl('doi:', x$text, ignore.case=TRUE)) {
-#handle DOI
-	} else {		
+	if(grepl('doi', x$text, ignore.case=TRUE)) {
+		#first open tree, then dryad, then treebase
+		#studies_find_studies(property="ot:studyPublication", value="http://dx.doi.org/10.1600/036364411X605092")
+		ot.result <- NULL
+		raw.doi <- gsub("doi:*\\s*", 'http://dx.doi.org/', x$text)
+		raw.doi <- gsub(".*#icanhaztree", "", raw.doi)
+		raw.doi <- gsub(".*#tree", "", raw.doi)
+		raw.doi <- gsub("\\s+","", raw.doi)
+		ot.returntweet <- NULL
+		dryad.returntweet <- NULL
+		treebase.returntweet <- NULL
+		try(ot.result <- rotl::studies_find_studies(property="ot:studyPublication", value=raw.doi))
+		if(!is.null(ot.result)) {
+			study.info <- NULL
+			try(study.info <- get_publication(get_study_meta(ot.result$study_ids))[1])
+			return.url <- paste0('https://tree.opentreeoflife.org/curator/study/view/', ot.result$study_ids[1], '/?tab=metadata')
+			return.tweet <- paste('@', x$screenName, " trees at ", return.url, " from @opentreeoflife via @phylotastic", ifelse(is.null(study.info),"", paste0(" from ", study.info)), sep="")
+			ot.returntweet <- strtrim(return.tweet, 280)
+			updateStatus(ot.returntweet, inReplyTo=x$replyToSID, bypassCharLimit=TRUE)
+
+		}
+		dryad.result <- NULL
+		try(dryad.result <- rdryad::d_solr_search(q=paste0("dc.relation.isreferencedby:", raw.doi) ,
+   fl="dc.identifier"))
+	 	if(!is.null(dryad.result)) {
+			if(nrow(dryad.result>0)) {
+				dryad.id <- strsplit(as.data.frame(dryad.result, stringsAsFactors=FALSE)[1,1], ',')[[1]][1]
+				return.url <- paste0('http://datadryad.org/resource/', dryad.id)
+				dryad.returntweet <- paste0('@', x$screenName, " data (perhaps not trees) at ", return.url, " from @datadryad via @phylotastic")
+				updateStatus(dryad.returntweet, inReplyTo=x$replyToSID, bypassCharLimit=TRUE)
+			}
+		}
+		treebase.result <- NULL
+		#try(treebase.result <- search_treebase(raw.doi, by="doi"))
+		# Note: this will fail due to https://github.com/ropensci/treeBASE/issues/15
+
+	} else {
 		substring <- gsub(".*#icanhaztree ", "", x$text, ignore.case=TRUE)
+		substring <- gsub(".*#tree ", "", substring, ignore.case=TRUE)
 		tnrs.result <- NULL
 		try(tnrs.result <- tnrs_match_names(names=substring))
 		if(!is.null(tnrs.result)) {
@@ -45,7 +82,7 @@ ProcessICanHazTree <- function(x) {
 				updateStatus(return.tweet, inReplyTo=x$replyToSID, bypassCharLimit=TRUE)
 			}
 		} else {
-				
+
 		}
 	}
 }
@@ -53,6 +90,7 @@ ProcessICanHazTree <- function(x) {
 #http://www.ncbi.nlm.nih.gov/nuccore/?term=anolis+carolinensis
 ProcessICanHazNCBI <- function(x) {
 	substring <- gsub(" ",'+', gsub(".*#icanhazncbi ", "", x$text, ignore.case=TRUE))
+	substring <- gsub(" ",'+', gsub(".*#ncbi ", "", substring, ignore.case=TRUE))
 	return.url <- paste('http://www.ncbi.nlm.nih.gov/nuccore/?term=', substring, sep="")
 	return.tweet <- paste('@', x$screenName, " ", return.url, " via @phylotastic", sep="")
 	updateStatus(return.tweet, inReplyTo=x$replyToSID, bypassCharLimit=TRUE)
@@ -61,15 +99,17 @@ ProcessICanHazNCBI <- function(x) {
 #http://eol.org/search?q=anolis+carolinensis&show_all=true
 ProcessICanHazEOL <- function(x) {
 	substring <- gsub(" ",'+', gsub(".*#icanhazeol ", "", x$text, ignore.case=TRUE))
-	return.url <- paste('http://eol.org/search?q=', substring, '&search=Go', sep="")
+	substring <- gsub(" ",'+', gsub(".*#eol ", "", substring, ignore.case=TRUE))
+  return.url <- paste('http://eol.org/search?q=', substring, '&search=Go', sep="")
 	return.tweet <- paste('@', x$screenName, " ", return.url, " from @eol via @phylotastic", sep="")
 	updateStatus(return.tweet, inReplyTo=x$replyToSID, bypassCharLimit=TRUE)
 }
 
 ProcessICanHazMap <- function(x, limit=2000) {
-        substring <- gsub(".*#icanhazmap ", "", x$text, ignore.case=TRUE)
+	substring <- gsub(".*#icanhazmap ", "", x$text, ignore.case=TRUE)
+	substring <- gsub(".*#map ", "", substring, ignore.case=TRUE)
         map.file <- NULL
-     	key <- NULL 
+     	key <- NULL
        	try(key <- ((name_suggest(substring))$key)[1])
         if(!is.null(key)) {
 			dat <- occ_search(taxonKey=key, return='data', limit=limit)
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # phylobot
 Interactive twitter bot to field phylogenetic or related queries
+
+Usage: tweet to @phylobot with a query
+
+## Trees
+
+This serves as an interface to Open Tree of Life using Phylotastic and the rotl package.
+
+`@phylobot #tree Formica` or `@phylobot #icanhaztree Formica` will get a link to a tree of *Formica* ants
+
+`@phylobot #tree doi:10.1111/evo.13117` will look for data or trees from that study. Note the doi format: add `doi:` and the the DOI string (no http, etc. just the string of numbers and letters).
+
+## Maps
+
+`@phylobot #map Formica` will give you a map of the first 2000 data points from GBIF.
+
+## NCBI
+
+`@phylobot #map Formica` will give you a link to that taxon at NCBI.
+
+## EOL
+
+`@phylobot #eol Formica` will give you a link to that taxon at EOL.
+
+## Jobs
+
+`@phylobot #Icanhazjob` will give you a link to the Ecology and Evolution jobs wiki.
+
+# Long term plans
+
+This is just a prototype now and somewhat brittle. Longer term plans are to make queries more flexible, serve up chronograms from datelife, and provide info to other services. A major goal is to have it record what papers are being asked for by DOI to gauge interest and start recording which papers are desired but which have not deposited their data.
+
+# Installation
+
+To run, `nohup R CMD main.R > /dev/null`. You'll need a file with keys, called `.secret.R`, in the same directory as `main.R`. Its contents:
+
+```
+twitteR::setup_twitter_oauth("STRING","STRING","STRING","STRING")
+```
+
+where the STRINGs are those used to authorize the bot to tweet (something like `3ryaj5uhrjaFc1989EK`).
